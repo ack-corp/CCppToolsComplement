41033d672d2070668c21f78175783c96fd01b758
achaisne <achaisne@f1br5s4.paris.42.school>
2026-04-02 18:34:33 +0200

    most of basic resolver proto ready. I hope gpt good with regex xD

--- srcs/vscodeIntegration/backend/sourcesHeadersMatcher/getSourceProto.py
+++ srcs/vscodeIntegration/backend/sourcesHeadersMatcher/getSourceProto.py
@@ -1,7 +1,23 @@
-def get_c_proto(line):
+def get_c_function_proto(line):
+def get_c_function_imp(line):
 
-def get_cpp_proto(line):
 
-def get_cpp_class(line):
+def get_cpp_function_proto(line):
+def get_cpp_function_imp(line):
+
+
+def get_cpp_class_proto(line):
     #return true if we enter a class, else false + the proto
+def get_cpp_class_imp(line):
 
+
+# 
+
+def get_macro_proto(line)
+def get_macro_imp(line)
+    
+def get_struct_proto(line)
+def get_struct_imp(line)
+    
+def get_typedef_proto(line)
+def get_typedef_imp(line)
